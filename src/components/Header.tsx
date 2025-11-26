77f03e6419112da88827a4ac728a1e66202194d7
Yaara Yizchaki <yaarayi@gmail.com>
2025-11-26 16:34:24 +0200

    changed website name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header>
-      <h1>שם האתר שלי</h1>
+      <h1>האתר של תמר ויערה</h1>
       <nav style={{ display: 'flex', gap: '20px' }}>
         <a href="/">דף הבית</a>
         <a href="/about">אודות</a>

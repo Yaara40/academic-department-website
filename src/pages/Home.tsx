ffb3e412801ba6932f8a09cd4d8b44d3a0cf962b
Yaara Yizchaki <yaarayi@gmail.com>
2025-12-12 08:23:44 +0200

    Changed home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import WelcomeBox from '../components/WelcomeBox';
+import CourseList from '../features/courses/components/CourseList';
 
 const Home = () => {
   return (
     <div style={{ direction: 'rtl' }}>
       <WelcomeBox />
+      <CourseList />
     </div>
   );
 };

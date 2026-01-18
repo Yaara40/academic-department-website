a3d681b7ab76482e6d1caf9a763c18d0ba46f982
Yaara Yizchaki <yaarayi@gmail.com>
2026-01-18 07:41:04 +0200

    nothing special

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,27 @@
 import { Box, Typography } from "@mui/material";
 import HomeList from "../features/home/components/HomeList.tsx";
-import ThemeToggle from "../components/ThemeToggle"; // הוספנו את הייבוא הזה
+import ThemeToggle from "../components/ThemeToggle";
 
 export default function Home() {
   return (
-    <Box sx={{ direction: 'rtl' }}>
+    <Box sx={{ direction: "rtl" }}>
       <Box sx={{ p: 3 }}>
-        
         {/* אזור הכותרת המעודכן עם הכפתור */}
-        <Box 
-          sx={{ 
-            display: 'flex', 
-            justifyContent: 'space-between', // זה מה שדוחף צדדים מנוגדים
-            alignItems: 'center', 
-            mb: 3 
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between", // זה מה שדוחף צדדים מנוגדים
+            alignItems: "center",
+            mb: 3,
           }}
         >
           {/* צד ימין: הכותרת */}
           <Box>
-            <Typography variant="h4" fontWeight={800} sx={{ color: 'text.primary' }}>
+            <Typography
+              variant="h4"
+              fontWeight={800}
+              sx={{ color: "text.primary" }}
+            >
               ניהול דף הבית
             </Typography>
             <Typography color="text.secondary">
